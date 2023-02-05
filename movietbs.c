96b2d8af247764d90de135f50ccfb7dbe6ed2143
Varun <76661061+VARUNvk1729@users.noreply.github.com>
2023-02-06 00:23:41 +0530

    Made few changes

--- movietbs.c
+++ movietbs.c
@@ -111,144 +111,146 @@ printf("!!!!!!!!!!!!!!!!!!KEEP PIN SAFELY!!!!!!!!!!!!!!!!!!!\n");
 printf("PINS ARE REQUIRED AT THE TIME OF CANCELATION OF SEATS\n");
 printf("------------------------------------------------------------------\n");
 }
+ //printf("hello");
 }
 void book_seat()
 {
-node *cn,*temp[20];
-int n,z,flag;
-char row;
-int seat;
-char ch;
-do
-{
-z=1;
-printf("\nEnter No Of Tickets u Want To Buy: ");
-scanf("%d",&n);
-
-printf("\nEnter Seat Numbers: \n");
-
-for(int i=1;i<=n;i++)
-{
-printf("NO ");
-printf("%d",i);
-printf(" = "); 
-scanf(" %c",&row);
-scanf("%d",&seat);
-cn=hn;
-while(cn->next!=hn)
-{
-if(cn->row_no==row && cn->seat_no==seat)
-{
-if(cn->book=='a')
-{
-cn->book='b';
-temp[z]=cn;
-z++;
-}
-else
-{
-printf("INVALID CHOISE!\n");
-printf("%c",cn->row_no);
-printf("%d",cn->seat_no);
-printf(" Seat is alredy reserved \n");
-
-}
-}
-cn=cn->next;
-}
-if(cn->row_no==row && cn->seat_no==seat)
-{
-if(cn->book=='a')
-{
-cn->book='b';
-temp[z]=cn;
-z++;
-}
-else
-{
-printf("INVALID CHOISE!\n");
-printf("%c",cn->row_no);
-printf("%d",cn->seat_no);
-printf(" Seat is alredy reserved\n");
-}
-}
-}
-
-display1(temp,z);
-printf("\n\nPRESS 1 To check Seat Status\n");
-printf("PRESS 2 To book other seat\n");
-printf("PRESS 3 To Exit BOOKING PORTAL\n");
-printf("\ninput: ");
-scanf("%c",&ch);
-if(ch==1)
-display();
-}while(ch=='2');
+    node *cn,*temp[20];
+    int n,z,flag;
+    char row;
+    int seat;
+    int ch1;
+    do
+    {
+    z=1;
+    printf("\nEnter No Of Tickets u Want To Buy: ");
+    scanf("%d",&n);
+    
+    printf("\nEnter Seat Numbers: \n");
+    
+    for(int i=1;i<=n;i++)
+    {
+    printf("NO ");
+    printf("%d",i);
+    printf(" = "); 
+    scanf(" %c",&row);
+    scanf("%d",&seat);
+    cn=hn;
+    while(cn->next!=hn)
+    {
+    if(cn->row_no==row && cn->seat_no==seat)
+    {
+    if(cn->book=='a')
+    {
+    cn->book='b';
+    temp[z]=cn;
+    z++;
+    }
+    else
+    {
+    printf("INVALID CHOISE!\n");
+    printf("%c",cn->row_no);
+    printf("%d",cn->seat_no);
+    printf(" Seat is alredy reserved \n");
+    
+    }
+    }
+    cn=cn->next;
+    }
+    if(cn->row_no==row && cn->seat_no==seat)
+    {
+    if(cn->book=='a')
+    {
+    cn->book='b';
+    temp[z]=cn;
+    z++;
+    }
+    else
+    {
+    printf("INVALID CHOISE!\n");
+    printf("%c",cn->row_no);
+    printf("%d",cn->seat_no);
+    printf(" Seat is alredy reserved\n");
+    }
+    }
+    }
+    display1(temp,z);
+    printf("\n\nPRESS 1 To check Seat Status\n");
+    printf("PRESS 2 To book other seat\n");
+    printf("PRESS 3 To Exit BOOKING PORTAL\n");
+    printf("\ninput: ");
+    char temp;
+   // scanf("%c",&temp);
+    scanf("%d",&ch1);
+    if(ch1==1);
+        // display();
+    }while(ch1==2);
 }
 
 void cancel()
 {
-char row;
-int ch;
-int seat,pin;
-node *cn;
-do
-{
-ch=1;
-cn=hn;
-printf("SEAT NUMBER :");
-scanf(" %c",&row);
-scanf("%d",&seat);
-printf("PIN :");
-scanf("%d",&pin);
-while(cn->next!=hn)
-{
-if(cn->row_no==row && cn->seat_no==seat && cn->pin==pin)
-{
-printf("Are you sure u want to cancle the Seat (y/n) ");
-char c;
-scanf(" %c",&c);
-if(c=='y'||c=='Y')
-{
-printf("SEAT CANCELED SUCCESFULLY!\n");
-cn->book='a';
-}
-}
-else if(cn->row_no==row && cn->seat_no==seat && cn->pin!=pin)
-{
-printf("invalid SEAT NUMBER && PIN combination!!!!\n");
-}
-cn=cn->next;
-}
-if(cn->row_no==row && cn->seat_no==seat && cn->pin==pin)
-{
-printf("Are you sure u want to cancle (y/n) ");
-char c;
-scanf("%c",&c);
-if(c=='y'||c=='Y')
-{
-printf("SEAT CANCELED SUCCESFULLY!\n");
-cn->book='a';
-}
-}
-else if(cn->row_no==row && cn->seat_no==seat && cn->pin!=pin)
-{
-printf("invalid SEAT NUMBER && PIN combination!!!!\n");
-}
-printf("\n\nPRESS 1 To Check Seat Status\n"); 
-printf("PRESS 2 To Cancle More Tickets\n");
-printf("PRESS 3 To Exit CANCELATION PORTAL\n");
-printf("\ninput: ");
-scanf("%d",&ch);
-if(ch==1);
-//display();
-}
-while(ch==2);
+    char row;
+    int ch;
+    int seat,pin;
+    node *cn;
+    do
+    {
+    ch=1;
+    cn=hn;
+    printf("SEAT NUMBER :");
+    scanf(" %c",&row);
+    scanf("%d",&seat);
+    printf("PIN :");
+    scanf("%d",&pin);
+    while(cn->next!=hn)
+    {
+    if(cn->row_no==row && cn->seat_no==seat && cn->pin==pin)
+    {
+    printf("Are you sure u want to cancle the Seat (y/n) ");
+    char c;
+    scanf(" %c",&c);
+    if(c=='y'||c=='Y')
+    {
+    printf("SEAT CANCELED SUCCESFULLY!\n");
+    cn->book='a';
+    }
+    }
+    else if(cn->row_no==row && cn->seat_no==seat && cn->pin!=pin)
+    {
+    printf("invalid SEAT NUMBER && PIN combination!!!!\n");
+    }
+    cn=cn->next;
+    }
+    if(cn->row_no==row && cn->seat_no==seat && cn->pin==pin)
+    {
+    printf("Are you sure u want to cancle (y/n) ");
+    char c;
+    scanf("%c",&c);
+    if(c=='y'||c=='Y')
+    {
+    printf("SEAT CANCELED SUCCESFULLY!\n");
+    cn->book='a';
+    }
+    }
+    else if(cn->row_no==row && cn->seat_no==seat && cn->pin!=pin)
+    {
+    printf("invalid SEAT NUMBER && PIN combination!!!!\n");
+    }
+    printf("\n\nPRESS 1 To Check Seat Status\n"); 
+    printf("PRESS 2 To Cancle More Tickets\n");
+    printf("PRESS 3 To Exit CANCELATION PORTAL\n");
+    printf("\ninput: ");
+    scanf("%d",&ch);
+    if(ch==1);
+        // display();
+    }
+    while(ch==2);
 }
 
 int main()
 {
 int ch;
-int c;
+int c=0;
 printf("\n\n\n");
 printf("                 WELCOME TO MOVIE TICKET BOOKING \n");
 printf("                         ONLINE  \n\n");
@@ -262,26 +264,30 @@ printf("PRESS 1-> BOOK TICKETS\n");
 printf("PRESS 2-> CANCEL TICKETS\n");
 printf("PRESS 3-> EXIT\n");
 printf("\ninput: ");
+// char temp;
+// scanf("%d",&temp);
 scanf("%d",&ch);
 switch(ch)
 {
-case 1:
-book_seat();
-display();
-break;
-case 2:
-cancel();
-display();
-break;
+    case 1:
+    book_seat();
+    display();
+    break;
+    case 2:
+    cancel();
+    display();
+    break;
+    }
+    
+    if(ch==3)
+    {
+        printf("\n\nPRESS 1 To Main Menu\n");
+        printf("PRESS 2 To Exit CINEMAX PORTAL\n");
+        printf("\ninput: ");
+        scanf("%d",&c);
+    }
 }
-if(ch!=3)
-{
-printf("\n\nPRESS 1 To Main Menu\n");
-printf("PRESS 2 To Exit CINEMAX PORTAL\n");
-printf("\ninput: ");
-scanf("%d",&c);
-}
-}while(c==1);
-printf("\n\n\n");
-return 0;
+    while(c!=2);
+    printf("\n\n\n");
+    return 0;
 }
